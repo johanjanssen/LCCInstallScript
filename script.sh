6cc319dbdc187da8db81317870901b3f88f85664
Johan Janssen <johanj@infosupport.com>
2016-11-22 21:28:44 +0100

    Quick Maven fix for now.

--- script.sh
+++ script.sh
@@ -9,9 +9,8 @@ sudo /etc/init.d/lirc stop
 sudo /etc/init.d/lirc start
 wget http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 sudo tar xvf apache-maven-3.3.9-bin.tar.gz  -C /opt
-export PATH=/opt/apache-maven-3.3.9/bin:$PATH
 git clone https://github.com/johanjanssen/LCC.git
-mvn -f ../LCC/pom.xml clean package
+/opt/apache-maven-3.3.9/bin/mvn -f /home/pi/LCC/pom.xml clean package
 sudo sed -i '/^exit/s/^exit/\/usr\/lib\/jvm\/jdk-8-oracle-arm32-vfp-hflt\/bin\/java -jar \/home\/pi\/LCC\/target\/*.jar \& \n&/' /etc/rc.local
 gcc -lpthread control.c -o /home/pi/LCC/infrared
 /usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/bin/java -jar /home/pi/LCC/target/*.jar
